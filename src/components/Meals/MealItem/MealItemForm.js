9595a5a140def282df15c2d90090338021b47875
Sammydepoj <samueladepoju86@yahoo.com>
2023-01-31 14:21:01 +0100

    FEAT:Added the cart reducer

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,21 +1,46 @@
-import Input from '../../UI/Input';
-import classes from './MealItemForm.module.css';
+import { useRef, useState } from "react";
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsvalid, setAmountIsValid] = useState(true);
+
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const numberEnteredAmount = +enteredAmount;
+
+    if (
+      enteredAmount.trim().lenght === 0 ||
+      numberEnteredAmount < 1 ||
+      numberEnteredAmount > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+  };
+
+  props.onAddToCart(numberEnteredAmount);
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
-        label='Amount'
+        ref={amountInputRef}
+        label="Amount"
         input={{
-          id: 'amount_' + props.id,
-          type: 'number',
-          min: '1',
-          max: '5',
-          step: '1',
-          defaultValue: '1',
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
         }}
       />
       <button>+ Add</button>
+      {!amountIsvalid && <p>Please enter a valid amount(1-5)</p>}
     </form>
   );
 };
