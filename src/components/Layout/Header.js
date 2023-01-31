4f0a2cf3391aaecc18f4d628a974568ce00e4cc8
Sammydepoj <samueladepoju86@yahoo.com>
2023-01-31 08:52:02 +0100

    FEAT:Added the cart reducer

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,6 @@
 import {Fragment} from 'react';
-import HeaderCartButton from './HeaderCartButton';
 
-import mealsImage from '../../assets/meals.jpg';
+import mealsImage from '../../components/assets/meals.jpg'
 import classes from './Header.module.css';
 import HeaderCartButton from './HeaderCartButton';
 const Header = (props) => {
@@ -9,7 +8,7 @@ const Header = (props) => {
     <Fragment>
         <header className={classes.header}>
             <h1>ReactMeals </h1>
-            <HeaderCartButton />
+            <HeaderCartButton onClick={props.onShowCart} />
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt=' a table full of delicious food!'/>
