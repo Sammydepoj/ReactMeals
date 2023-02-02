d33e3f47febbb703967e8dde80592f199bb79e21
Sammydepoj <samueladepoju86@yahoo.com>
2023-02-02 08:19:52 +0100

    Feat: cart adding and removing functionality implemented

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import { useContext } from 'react';
-import CartContext from '../../store/cart-context';
-import CartIcon from '../Cart/CartIcon';
-import classes from './HeaderCartButton.module.css'
+import React from "react";
+import { useContext, useEffect, useState } from "react";
+import CartContext from "../../store/cart-context";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems= cartCtx.items.reduce((curNumber,item)=>{
-    return curNumber + item.amount
-  }, 0)
-  return (
-    <button className={classes.button} onClick={props.onClick}>
-        <span className={classes.icon}>
-            <CartIcon />
-        </span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>{numberOfCartItems}</span>
-    </button>
-  )
-}
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+  const btnClasses = `${classes.button} ${
+    buttonIsHighlighted ? classes.bump : ""
+  }`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    const timer = setButtonIsHighlighted(true);
+    setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
 
-export default HeaderCartButton
+    return() => {
+
+      clearTimeout(timer);
+    }
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
