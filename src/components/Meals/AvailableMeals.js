9982f56762df208777dd0daec344c593bc0d2262
Sammydepoj <samueladepoju86@yahoo.com>
2023-02-05 16:50:58 +0100

    feature:handled the isLoading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,6 +5,8 @@ import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
@@ -23,10 +25,17 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
     fetchMeals();
   }, []);
-
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
   const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
@@ -36,6 +45,7 @@ const AvailableMeals = () => {
       price={meal.price}
     />
   ));
+  // const showMeal = mealsList.length === 0 ? <p>Loading...</p> : mealsList;
 
   return (
     <section className={classes.meals}>
